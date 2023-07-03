efbe8bde9c99425a81d8979e6f5de2890c083c38
Aliaksandr Hrynko <san4o.mnk@gmail.com>
2023-07-03 18:19:44 +0300

    update libs

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
 
-SET msbuild="c:\Program Files\Microsoft Visual Studio\2022\Professional\Msbuild\Current\Bin\MSBuild.exe"
+SET msbuild="c:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\MSBuild.exe"
 
-%msbuild% "%CD%\Library.sln" /m /t:restore /t:pack /p:Configuration="Release" /p:Platform="Any Cpu" /p:VersionPrefix="2.0.0.0"
+%msbuild% "%CD%\Library.sln" /m /t:restore /t:pack /p:Configuration="Release" /p:Platform="Any Cpu" /p:VersionPrefix="2.0.1"
 
 move bin\Release\*.nupkg ../nuget/
